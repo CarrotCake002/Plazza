b00af01f6323e07ff1ebafeecaf779b9c6f3806e
CarrotCake <pol.siles-poloepitech.eu>
2026-07-26 20:55:52 +0200

    [MDF] testing for more accurate coverage results

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,5 +12,12 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test-setup.js'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{js,jsx}'],
+      exclude: ['src/test-setup.js'],
+    },
   },
 })
